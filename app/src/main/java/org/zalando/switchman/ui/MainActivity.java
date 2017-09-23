51c88abedc70a28d60aa2c57c01dde2045b76825
Nikita Kozlov <nikita.e.kozlov@gmail.com>
2017-09-23 19:02:10 +0200

    Applying "Move Method" to move `getRecommendationListener()` to `Presenter` Fixed visibily issues for `onRecommendationRequestSuccess()` and `onRecommendationRequestFail()`

--- app/src/main/java/org/zalando/switchman/ui/MainActivity.java
+++ app/src/main/java/org/zalando/switchman/ui/MainActivity.java
@@ -6,14 +6,11 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
-import org.zalando.switchman.ItemId;
 import org.zalando.switchman.R;
 import org.zalando.switchman.Recipe;
 import org.zalando.switchman.repo.Response;
 
 import java.util.List;
-
-import rx.android.schedulers.AndroidSchedulers;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
@@ -50,41 +47,14 @@ public class MainActivity extends AppCompatActivity {
         presenter.getSubscription().clear();
     }
 
-    RecommendationView.RecommendationListener getRecommendationListener() {
-        return new RecommendationView.RecommendationListener() {
-
-            @Override
-            public void recommend(ItemId itemId, RecommendationView.RequestListener requestListener) {
-                presenter.getSubscription().add(presenter.getRecommendationDataSource().addItem(itemId)
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(
-                                response -> onRecommendationRequestSuccess(response, requestListener,
-                                        "Due to an error request to start recommending is failed"),
-                                throwable -> onRecommendationRequestFail(requestListener, throwable.getMessage())
-                        ));
-            }
-
-            @Override
-            public void unrecommend(ItemId itemId, RecommendationView.RequestListener requestListener) {
-                presenter.getSubscription().add(presenter.getRecommendationDataSource().removeItem(itemId)
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(
-                                response -> onRecommendationRequestSuccess(response, requestListener,
-                                        "Due to an error request to stop recommending is failed"),
-                                throwable -> onRecommendationRequestFail(requestListener, throwable.getMessage())
-                        ));
-            }
-        };
-    }
-
-    private void onRecommendationRequestFail(RecommendationView.RequestListener requestListener, String message) {
+    void onRecommendationRequestFail(RecommendationView.RequestListener requestListener, String message) {
         requestListener.onFail();
         showNotification(message);
     }
 
-    private void onRecommendationRequestSuccess(Response response,
-                                                RecommendationView.RequestListener requestListener,
-                                                String errorNotification) {
+    void onRecommendationRequestSuccess(Response response,
+                                        RecommendationView.RequestListener requestListener,
+                                        String errorNotification) {
         if (response.isSuccessful()) {
             requestListener.onSuccess();
         } else if (!response.isSkipped()) {
--- app/src/main/java/org/zalando/switchman/ui/Presenter.java
+++ app/src/main/java/org/zalando/switchman/ui/Presenter.java
@@ -1,9 +1,11 @@
 package org.zalando.switchman.ui;
 
 import org.zalando.switchman.Injector;
+import org.zalando.switchman.ItemId;
 import org.zalando.switchman.data.RecommendationDataSource;
 import org.zalando.switchman.data.SearchDataSource;
 
+import rx.android.schedulers.AndroidSchedulers;
 import rx.subscriptions.CompositeSubscription;
 
 public class Presenter {
@@ -43,10 +45,37 @@ public class Presenter {
 
     void search(MainActivity mainActivity) {
         getSubscription().add(getSearchDataSource().search()
-                .subscribe(recipes -> mainActivity.displayRecipes(recipes, mainActivity.presenter.getRecommendationStateChecker(), mainActivity.getRecommendationListener())));
+                .subscribe(recipes -> mainActivity.displayRecipes(recipes, mainActivity.presenter.getRecommendationStateChecker(), mainActivity.presenter.getRecommendationListener(mainActivity))));
     }
 
     RecommendationStateChecker getRecommendationStateChecker() {
         return new RecommendationStateChecker(getRecommendationDataSource());
+    }
+
+    RecommendationView.RecommendationListener getRecommendationListener(MainActivity mainActivity) {
+        return new RecommendationView.RecommendationListener() {
+
+            @Override
+            public void recommend(ItemId itemId, RecommendationView.RequestListener requestListener) {
+                getSubscription().add(getRecommendationDataSource().addItem(itemId)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                response -> mainActivity.onRecommendationRequestSuccess(response, requestListener,
+                                        "Due to an error request to start recommending is failed"),
+                                throwable -> mainActivity.onRecommendationRequestFail(requestListener, throwable.getMessage())
+                        ));
+            }
+
+            @Override
+            public void unrecommend(ItemId itemId, RecommendationView.RequestListener requestListener) {
+                getSubscription().add(getRecommendationDataSource().removeItem(itemId)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                response -> mainActivity.onRecommendationRequestSuccess(response, requestListener,
+                                        "Due to an error request to stop recommending is failed"),
+                                throwable -> mainActivity.onRecommendationRequestFail(requestListener, throwable.getMessage())
+                        ));
+            }
+        };
     }
 }
